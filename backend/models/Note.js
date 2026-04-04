eaad59b08a5324643fec54cafd12a62908f14944
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-04 18:24:41 +0530

    Notes Schema

---
+++ backend/models/Note.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
