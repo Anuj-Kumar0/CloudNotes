968acb131c6687166a10ab2dce66305e31420923
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-04 18:20:08 +0530

    Register Component

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await API.post("/auth/register", form);
+
+    localStorage.setItem("user", JSON.stringify(res.data));
+    navigate("/dashboard");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+
+      <input
+        placeholder="Name"
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
+      />
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Register;
