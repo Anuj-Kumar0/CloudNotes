78b1f79ec509cd52f00e9b09665916ae6a1ac6ac
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-04 18:25:38 +0530

    server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 
 dotenv.config();
 
@@ -10,10 +13,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// connect database
+connectDB();
+
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+app.use("/api/auth", authRoutes);
+app.use("/api/notes", noteRoutes);
 
 const PORT = process.env.PORT || 5000;
 
