78b7c792d59c96276d4fa153b47741295e30b335
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-05 08:59:26 +0530

    feat: Added framer-motion & title

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,33 +5,41 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const [form, setForm] = useState({ email: "", password: "" });
   const navigate = useNavigate();
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const res = await API.post("/auth/login", form);
-
     localStorage.setItem("user", JSON.stringify(res.data));
     navigate("/dashboard");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+    <div className="container">
+      <h1 className="title">CloudNotes</h1>
 
-      <input
-        placeholder="Email"
-        onChange={(e) => setForm({ ...form, email: e.target.value })}
-      />
+      <div className="glass">
+        <form onSubmit={handleSubmit}>
+          <h2>Login</h2>
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setForm({ ...form, password: e.target.value })}
-      />
+          <input
+            placeholder="Email"
+            onChange={(e) =>
+              setForm({ ...form, email: e.target.value })
+            }
+          />
 
-      <button type="submit">Login</button>
-    </form>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setForm({ ...form, password: e.target.value })
+            }
+          />
+
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,40 +3,54 @@ import API from "../services/api";
 import { useNavigate } from "react-router-dom";
 
 function Register() {
-  const [form, setForm] = useState({ name: "", email: "", password: "" });
-  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
 
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const res = await API.post("/auth/register", form);
-
     localStorage.setItem("user", JSON.stringify(res.data));
     navigate("/dashboard");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Register</h2>
+    <div className="container">
+      <h1 className="title">CloudNotes</h1>
 
-      <input
-        placeholder="Name"
-        onChange={(e) => setForm({ ...form, name: e.target.value })}
-      />
+      <div className="glass">
+        <form onSubmit={handleSubmit}>
+          <h2>Register</h2>
 
-      <input
-        placeholder="Email"
-        onChange={(e) => setForm({ ...form, email: e.target.value })}
-      />
+          <input
+            placeholder="Name"
+            onChange={(e) =>
+              setForm({ ...form, name: e.target.value })
+            }
+          />
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setForm({ ...form, password: e.target.value })}
-      />
+          <input
+            placeholder="Email"
+            onChange={(e) =>
+              setForm({ ...form, email: e.target.value })
+            }
+          />
 
-      <button type="submit">Register</button>
-    </form>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setForm({ ...form, password: e.target.value })
+            }
+          />
+
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
